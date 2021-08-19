a64a956048e27a12b6e60e7d5f00d5d7fc80c4b4
MinHie <55249566+leminhhieust@users.noreply.github.com>
2021-08-19 23:13:44 +0700

    Add all models

--- src/main/java/com/harry/news_jdbc/model/UserModel.java
+++ src/main/java/com/harry/news_jdbc/model/UserModel.java
@@ -1,8 +1,11 @@
 package com.harry.news_jdbc.model;
 
-public class UserModel {
+public class UserModel extends AbstractModel<UserModel> {
     private String userName;
     private String fullName;
+    private String password;
+    private int status;
+    private Long roleId;
 
     public String getUserName() {
         return userName;
@@ -18,5 +21,29 @@ public class UserModel {
 
     public void setFullName(String fullName) {
         this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
     }
 }
